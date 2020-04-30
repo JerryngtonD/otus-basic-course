9b833efffc56c1bb12928617dc759c680a1bec4e
jerryngton <jerryngtond@gmail.com>
2020-04-30 16:11:45 +0300

    intializing film moved to application class

--- app/src/main/java/ru/otus/cineman/activity/PreviewMoviesActivity.kt
+++ app/src/main/java/ru/otus/cineman/activity/PreviewMoviesActivity.kt
@@ -16,6 +16,7 @@ import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import mu.KLogging
+import ru.otus.cineman.MovieStorage
 import ru.otus.cineman.R
 import ru.otus.cineman.adapter.MovieItemAdapter
 import ru.otus.cineman.animation.CustomItemAnimator
@@ -101,7 +102,7 @@ class PreviewMoviesActivity : AppCompatActivity() {
             savedInstanceState.getParcelableArrayList<MovieItem>(FILMS_STORED)
                 ?.toMutableList() ?: throw Exception()
         } else {
-            initializeMovies()
+            MovieStorage.getMovieStorage()
         }
 
         initRecycler()
@@ -176,101 +177,6 @@ class PreviewMoviesActivity : AppCompatActivity() {
         sharedPreferences.edit().apply {
             putBoolean(KEY_IS_NIGHT_MODE, isCheckedNightMode)
         }.apply()
-    }
-
-    private fun initializeMovies(): MutableList<MovieItem> {
-        return mutableListOf(
-            MovieItem(
-                titleId = R.string.spider_man_title,
-                imageId = R.drawable.spiderman,
-                descriptionId = R.string.spider_man_description
-            ),
-            MovieItem(
-                titleId = R.string.hulk_title,
-                imageId = R.drawable.hulk,
-                descriptionId = R.string.hulk_description
-            ),
-            MovieItem(
-                titleId = R.string.batman_title,
-                imageId = R.drawable.batman,
-                descriptionId = R.string.batman_description
-            ),
-            //---------------------------------------------------//
-            MovieItem(
-                titleId = R.string.antman_title,
-                imageId = R.drawable.antman,
-                descriptionId = R.string.antman_description
-            ),
-            MovieItem(
-                titleId = R.string.artas_title,
-                imageId = R.drawable.artas,
-                descriptionId = R.string.artas_description
-            ),
-            MovieItem(
-                titleId = R.string.asterix_title,
-                imageId = R.drawable.asterix,
-                descriptionId = R.string.asterix_description
-            ),
-            //---------------------------------------------------//
-            MovieItem(
-                titleId = R.string.bart_title,
-                imageId = R.drawable.bart,
-                descriptionId = R.string.bart_description
-            ),
-            MovieItem(
-                titleId = R.string.batman_title,
-                imageId = R.drawable.batman,
-                descriptionId = R.string.batman_description
-            ),
-            MovieItem(
-                titleId = R.string.cat_woman_title,
-                imageId = R.drawable.cat_woman,
-                descriptionId = R.string.cat_woman_description
-            ),
-            //---------------------------------------------------//
-            MovieItem(
-                titleId = R.string.cool_man_title,
-                imageId = R.drawable.cool_man,
-                descriptionId = R.string.cool_man_description
-            ),
-            MovieItem(
-                titleId = R.string.doctor_strange_title,
-                imageId = R.drawable.doctor_strange,
-                descriptionId = R.string.doctor_strange_description
-            ),
-            MovieItem(
-                titleId = R.string.gomer_title,
-                imageId = R.drawable.gomer,
-                descriptionId = R.string.gomer_description
-            ),
-            //---------------------------------------------------//
-            MovieItem(
-                titleId = R.string.hulk_title,
-                imageId = R.drawable.hulk,
-                descriptionId = R.string.hulk_description
-            ),
-            MovieItem(
-                titleId = R.string.mummy_title,
-                imageId = R.drawable.mummy,
-                descriptionId = R.string.mummy_description
-            ),
-            MovieItem(
-                titleId = R.string.spider_man_title,
-                imageId = R.drawable.spiderman,
-                descriptionId = R.string.spider_man_description
-            ),
-            //---------------------------------------------------//
-            MovieItem(
-                titleId = R.string.tanos_title,
-                imageId = R.drawable.tanos,
-                descriptionId = R.string.tanos_description
-            ),
-            MovieItem(
-                titleId = R.string.venom_title,
-                imageId = R.drawable.venom,
-                descriptionId = R.string.venom_description
-            )
-        )
     }
 
     private fun initRecycler() {
