0a15f1a6bf134e23aeb313d12ffa617f9c0ee4de
jerryngton <jerryngtond@gmail.com>
2020-05-05 17:02:08 +0300

    - added navigation drawer

--- app/src/main/java/ru/otus/cineman/activity/MainActivity.kt
+++ app/src/main/java/ru/otus/cineman/activity/MainActivity.kt
@@ -1,13 +1,21 @@
 package ru.otus.cineman.activity
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import ru.otus.cineman.MovieStorage.Companion.getFavoriteMovieStorage
 import ru.otus.cineman.R
 import ru.otus.cineman.fragment.*
 import ru.otus.cineman.model.MovieItem
 
 
-class MainActivity : AppCompatActivity(), MovieListListener, MovieDetailsListener {
+class MainActivity : AppCompatActivity(), MovieListListener, MovieDetailsListener, NavigationView.OnNavigationItemSelectedListener {
     companion object {
         const val TAG = "MovieListFragment"
 
@@ -16,6 +24,9 @@ class MainActivity : AppCompatActivity(), MovieListListener, MovieDetailsListene
         const val UPDATED_COMMENT = "UPDATED_COMMENT"
         const val UPDATED_IS_LIKED_STATUS = "UPDATED_IS_LIKED_STATUS"
     }
+
+    private var drawer: DrawerLayout? = null
+    private var toolbar: Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +38,18 @@ class MainActivity : AppCompatActivity(), MovieListListener, MovieDetailsListene
                 .replace(R.id.fragmentContainer, MoviesListFragment(), TAG)
                 .commit()
         }
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        drawer = findViewById(R.id.drawer_layout)
+        toolbar = findViewById(R.id.toolbar)
+
+        val navigationView = findViewById<NavigationView>(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener(this)
+
+        val toggle = ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer?.addDrawerListener(toggle)
+        toggle.syncState()
     }
 
     private fun openMovieDetailsFragment(movieItem: MovieItem) {
@@ -60,13 +83,35 @@ class MainActivity : AppCompatActivity(), MovieListListener, MovieDetailsListene
     }
 
     override fun openFavoriteMovies() {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(
-                R.id.fragmentContainer, MoviesListFavoriteFragment(), null
-            )
-            .addToBackStack(null)
-            .commit()
+        if (getFavoriteMovieStorage().size != 0) {
+            supportFragmentManager
+                .beginTransaction()
+                .replace(
+                    R.id.fragmentContainer, MoviesListFavoriteFragment(), null
+                )
+                .addToBackStack(null)
+                .commit()
+        } else {
+            Toast.makeText(this, R.string.favorites_empty, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (drawer?.isDrawerOpen(GravityCompat.START) == true) {
+            drawer?.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.nav_favorites -> openFavoriteMovies()
+
+            else -> Toast.makeText(this, R.string.share, Toast.LENGTH_SHORT).show()
+        }
+        drawer?.closeDrawer(GravityCompat.START)
+        return true
     }
 
     //    companion object : KLogging() {
